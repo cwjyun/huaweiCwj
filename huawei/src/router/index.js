285db6a4f67bce1e633f2ab117adeb8f88bdb10b
cwjyun <cwjyun@163.com>
2020-06-29 15:10:18 +0800

    11111111111

--- huawei/src/router/index.js
+++ huawei/src/router/index.js
@@ -5,7 +5,7 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',//去掉路由中的#号
+  mode: 'history',
   routes: [
     {
       path: '/',
