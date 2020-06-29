e898fcaf7884d39034172f21405301d30ac02d07
cwjyun <cwjyun@163.com>
2020-06-29 16:35:19 +0800

    11111111111

--- huawei/src/components/common/foot.vue
+++ huawei/src/components/common/foot.vue
@@ -1,4 +1,20 @@
 <template>
-  <div class="foot">
-  </div>
+  <footer class="footer" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <p class="mb-1">Copyright &copy; 2020.Company name All rights reserved.<a target="_blank" href="http://sc.chinaz.com/moban/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a></p>
+          <div class="credits">
+
+          </div>
+        </div>
+        <div class="col-sm-6 social text-md-right">
+          <a href="#"><span class="icofont-twitter"></span></a>
+          <a href="#"><span class="icofont-facebook"></span></a>
+          <a href="#"><span class="icofont-dribbble"></span></a>
+          <a href="#"><span class="icofont-behance"></span></a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
--- huawei/src/main.js
+++ huawei/src/main.js
@@ -4,6 +4,34 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import './assets/img/favicon.png'
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+import './assets/vendor/icofont/icofont.min.css'
+import './assets/vendor/aos/aos.css'
+import './assets/vendor/owlcarousel/assets/owl.carousel.min.css'
+import './assets/css/style.css'
+
+import './assets/vendor/jquery/jquery.min.js'
+import './assets/vendor/jquery/jquery-migrate.min.js'
+import './assets/vendor/easing/easing.min.js'
+// import './assets/vendor/isotope/isotope.pkgd.min.js'
+import './assets/vendor/aos/aos.js'
+import './assets/vendor/owlcarousel/owl.carousel.js'
+// import './assets/js/main.js'
+
+// <!-- Vendor JS Files -->
+// <script src="vendor/jquery/jquery.min.js"></script>
+//   <script src="vendor/jquery/jquery-migrate.min.js"></script>
+//   <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+//   <script src="vendor/easing/easing.min.js"></script>
+//
+//   <script src="vendor/isotope/isotope.pkgd.min.js"></script>
+//   <script src="vendor/aos/aos.js"></script>
+//   <script src="vendor/owlcarousel/owl.carousel.min.js"></script>
+//
+//   <!-- Template Main JS File -->
+//   <script src="js/main.js"></script>
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
